59917c1ec44b89f6bcb7ad6be6c77480708844eb
ikstv <tankoviktor@icloud.com>
2026-09-18 23:49:16 +0300

    build(android): add installable preview variant

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -46,6 +46,12 @@ android {
     }
 
     buildTypes {
+        create("preview") {
+            initWith(getByName("debug"))
+            applicationIdSuffix = ".preview"
+            versionNameSuffix = "-preview"
+        }
+
         release {
             // First release ships unminified on purpose: minify + resource
             // shrinking need a ProGuard pass verified on-device (ktor +
