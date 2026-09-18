7cb45888e4f79335ef31e9637af798a1805e448d
ikstv <tankoviktor@icloud.com>
2026-09-18 23:53:47 +0300

    build(android): label preview as 0.1.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -49,7 +49,6 @@ android {
         create("preview") {
             initWith(getByName("debug"))
             applicationIdSuffix = ".preview"
-            versionNameSuffix = "-preview"
         }
 
         release {
